011a7f8662fdc770853575eeb39239b9cc85df56
yinjiajun <316390862@qq.com>
2026-08-18 15:14:31 +0800

    feat: 添加 AGENTS.md 文件和聊天模型实现

---
+++ src/02-prompt-engineering.js
@@ -0,0 +1,44 @@
+import { ChatOpenAI } from '@langchain/openai'
+import { config } from 'dotenv'
+import { readFile } from 'fs/promises'
+import { input } from '@inquirer/prompts'
+
+// 这里用 new URL 来获取的原因是不同目录下启动的 node 进程，__dirname 的值不一样，导致路径不对
+const systemPrompt = await readFile(new URL('../prompts/AGENTS.md', import.meta.url), 'utf-8')
+
+// 加载环境变量
+config()
+
+const chatModel = new ChatOpenAI({
+  // 模型名
+  model: process.env.AI_MODEL,
+  // 额外传递给模型的参数
+  modelKwargs: {
+    // 关闭深度思考
+    enable_thinking: false,
+  },
+  configuration: {
+    // 模型厂商提供的 baseURL
+    baseURL: process.env.AI_BASE_URL,
+    // 你自己的 API KEY
+    apiKey: process.env.AI_API_KEY,
+  },
+})
+
+// 在命令行里输入文本
+const content = await input({ message: '你 >' })
+
+// 给模型发送消息
+const result = await chatModel.invoke([
+  {
+    role: 'system',
+    content: systemPrompt,
+  },
+  {
+    role: 'user',
+    // 把命令行里面你叽里呱啦输入的内容发送给大模型
+    content: content,
+  },
+])
+
+console.log(result.content) // 看到模型叽里呱啦的输出
